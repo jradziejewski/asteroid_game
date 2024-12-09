f1340cc86f18b363a09c223ceb2e6aa31f784d9c
jradziejewski <192462@student.ue.wroc.pl>
2024-12-09 18:55:19 +0100

    refactor a bid and add clock

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, color)
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
+
 
 if __name__ == "__main__":
     main()
